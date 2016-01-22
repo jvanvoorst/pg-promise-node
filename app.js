7d95a91dc2a4120eb649505ebb4504bcc7860927
Justin <justin.vanvoorst@gmail.com>
2016-01-22 16:03:00 -0700

    testing db connection

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ var connection = require('./connection.js');
 var pgp = require('pg-promise');
 
 var db = pgp(connection);
+
+db.query('SELECT NOW() AS "theTime"')
+	.then(function (data) {
+		console.log(data);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
