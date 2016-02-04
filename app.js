bfdf523a76d7b5d24ce4b4f9968e7d67733b84f6
Justin <justin.vanvoorst@gmail.com>
2016-02-04 15:07:19 -0700

    adding sql query

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var pgp = require('pg-promise')({
 
 var db = pgp(connection);
 
-db.query('SELECT NOW() AS "theTime"')
+var query = 'SELECT rm.* FROM ".SIERRA_record_metadata." INNER JOIN ".SIERRA_bib_view." ON rm.record_num = bv.record_num WHERE rm.record_last_updated_gmt > '".$date."' AND rm.record_type_code = 'b' AND bv.bcode3 = '-' ORDER BY rm.record_last_updated_gmt ASC;'
+
+db.query(query)
 	.then(function (data) {
 		console.log(data);
 	})
