8c832aeba8a0e930dbc4a53f5d01164721fb71ea
Justin <justin.vanvoorst@gmail.com>
2016-02-05 16:41:38 -0700

    more work on sql statement

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ var pgp = require('pg-promise')({
 
 var db = pgp(connection);
 
-var query = 'SELECT rm.* FROM ".SIERRA_record_metadata." INNER JOIN ".SIERRA_bib_view." ON rm.record_num = bv.record_num WHERE rm.record_last_updated_gmt > '".$date."' AND rm.record_type_code = 'b' AND bv.bcode3 = '-' ORDER BY rm.record_last_updated_gmt ASC;'
+var date = new Date('2016, 1, 15');
 
-db.query(query)
+// var query = "SELECT rm.* FROM ".SIERRA_record_metadata." INNER JOIN ".SIERRA_bib_view." ON rm.record_num = bv.record_num WHERE rm.record_last_updated_gmt > $/date/ AND rm.record_type_code = $b AND bv.bcode3 = $- ORDER BY rm.record_last_updated_gmt ASC LIMIT 10;";
+// var querySimple = "SELECT rm.* FROM ".SIERRA_record_metadata" LIMIT 5";
+
+
+db.query("SELECT * FROM SIERRA_VIEW.record_metadata LIMIT 5")
 	.then(function (data) {
 		console.log(data);
 	})
 	.catch(function (error) {
+		console.log('Error :')
 		console.log(error);
 	});
